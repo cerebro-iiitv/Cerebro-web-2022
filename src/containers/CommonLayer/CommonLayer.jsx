0ec995d388c217ca6febdb25be6c163031207e37
rashiyadhruv <202051153@iiitvadodara.ac.in>
2022-02-17 00:38:42 +0530

    adds dashboard button functionality

--- src/containers/CommonLayer/CommonLayer.jsx
+++ src/containers/CommonLayer/CommonLayer.jsx
@@ -1,8 +1,57 @@
 import Navbar from "../Navbar/Navbar";
 import "./CommonLayer.scss";
 import "../Navbar/Navbar.scss";
-
+import React from "react";
+import dashboard from "../../assets/images/dashboard icon.svg";
 const CommonLayer = () => {
+
+
+  function LoginButton(props) {
+    return (
+      //  <Link to="/login">
+      <>
+        <p id="lgndash" onClick={props.onClick}>LOGIN / SIGNUP</p>
+        </>
+      //  </Link>
+      
+    );
+  }
+  function Dashboard(){
+    return(
+      //  <Link to="/dashboard">
+      <>
+       <img src={dashboard} alt="dashboard"></img> <p id="lgndash">DASHBOARD</p>
+      
+      </>
+      //  </Link>
+    );
+  }
+
+  class LogStatus extends React.Component{
+    constructor(props) {
+      super(props);
+      this.state = {isLoggedIn: false};
+    }
+  
+    
+
+    render(){
+      const isLoggedIn = this.state.isLoggedIn;
+    let button;
+    if (isLoggedIn) {
+      button = <Dashboard/>;
+    } else {
+      button = <LoginButton/>;
+    }
+      return(
+        <div className="dashboard-content">
+        {button}
+      </div>
+      )
+    }
+  } 
+
+
   return (
     <div className="main-wrapper">
       <div className="blur-layer"></div>
@@ -11,7 +60,7 @@ const CommonLayer = () => {
           <div className="visible-layer-top-spacer"></div>
           <div className="visible-layer-top-center"></div>
           <div className="visible-layer-top-dashboard">
-            <div className="visible-layer-top-dashboard-main">DASHBOARD</div>
+            <div className="visible-layer-top-dashboard-main"><LogStatus/></div>
           </div>
         </div>
           <Navbar status="hover" />
