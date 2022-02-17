16aeddeac8434d16e5f0af529c29f0bf14a1198b
Dhruv Dave <78278281+DhruvDave12@users.noreply.github.com>
2022-02-17 18:46:39 +0530

    add responsive view and made card more responsive

--- src/components/particular-event/particular-event.component.jsx
+++ src/components/particular-event/particular-event.component.jsx
@@ -33,48 +33,65 @@ export const ParticularEvent = ({ title, start_time, prize, end_time, descriptio
 
         <div className='time-line'>
           <div className='date1'>
-            <p className='date1-items'>{start_time} <br />
-              <img src={dateLine} alt="date-line" className='date-line' /> <br />
-              8:00PM</p>
+            <p className='date1-items'>{start_time} </p>
+            <img src={dateLine} alt="date-line" className='date-line' />
+            <p> 8:00PM</p>
           </div>
           <img src={toDot} alt="to-dot" className='to-dot' />
           <div className='date1'>
-            <p className='date1-items'>{end_time} <br />
-              <img src={dateLine} alt="date-line" className='date-line' /> <br />
-              8:00PM</p>
+            <p className='date1-items'>{end_time} </p>
+            <img src={dateLine} alt="date-line" className='date-line' />
+             <p>8:00PM</p>
           </div>
         </div>
 
 
         <div className="event-detail">
-          <div className="prize-queries">
-            <h4 className='prize-worth-query'>Prize Worth</h4>
-            <h4 className='team-size-query'>Team Size</h4>
-            <h4 className='rules-regulations'> <a href="https://discord.gg/QaYWmeBc">Rules and Regulations</a> <br />
-              <a href="https://discord.gg/QaYWmeBc">Social Media</a>
-            </h4>
+          <div className="left-event-details">
+            <div className="prize-sec">
+              <p className="prize-worth-query">Prize Worth</p>
+              <div className="prize-dets">
+                <p className="prize-worth-detail">{prize} INR</p>
+              </div>
+            </div>
+
+            <div className="team-sec">
+              <p className="team-size-query">Team Size</p>
+              <div className="team-dets">
+                <p className="team-size-detail">{team_size}</p>
+              </div>
+            </div>
+
+            <div className="rules">
+              <p className='rules-regulations'> <a href="https://discord.gg/QaYWmeBc">Rules and Regulations</a> <br />
+                <a href="https://discord.gg/QaYWmeBc">Social Media</a>
+              </p>
+            </div>
           </div>
 
-        
-          <div className="prize-details">
-              <h4 className='prize-worth-detail'>{prize} INR</h4>
-              <h4 className='team-size-detail'>{team_size}</h4>
+
+          <div className="mid-line"></div>
+
+          <div className="right-event-container">
+              <div className="convenor-sec">
+                <p className='conv'>Convenor</p>
+                <div className="convenor-dets">
+                  <p>{convenor}</p>
+                </div>
+              </div>
+              <div className="co-convenor-sec">
+                <p className='coconv'>Co Convenor</p>
+                <p className='co_convenor-details'>{co_convenor1}, {co_convenor2}</p>
+              </div>
+              <div className="members-sec">
+                <p className='mems'>Members</p>
+                <p className='members-details'>{mem1}, {mem2}</p>
+              </div>
           </div>
 
-          <div className='mid-line'></div>
-
-          <div className="organizers-queries">
-              <h4>Convenor</h4>
-              <h4>Co-Convenor</h4>
-              <h4 id="members">Members</h4>
-          </div>
-
-          <div className="organizers-details">
-            <p>{convenor}</p>
-            <p>{co_convenor1}, {co_convenor2}</p>
-            <p>{mem1}, {mem2}</p>
-          </div>
         </div>
+
+
         <form>
           <div className='button-container'>
             <button id="single-button">Register</button>
