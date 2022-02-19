9f5f7c62dcb52fa98734660340d4eb16db4a636d
rashiyadhruv <81905828+rashiyadhruv@users.noreply.github.com>
2022-02-19 21:53:00 +0530

    Layout (#7)
    * Add resized and responsive navbar
    
    * Adds hover effect to the navbar , and responsive from 1024px to 1540px
    
    * Adds functionality made by madhur
    
    * Adds complete hover effect
    
    * adds dashboard button functionality
    
    * Adds blur layer on hover
    
    * almost complete
    
    * hover state fixed
    
    * All done except adding layout in routing
    
    * Adds Layout route
    
    * Final commit
    
    * Removed unnecesary import
    
    Co-authored-by: batflarrow <dwivedia2001@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
+import MainLayout from "./containers/Layout/Layout";
 import React, { useContext } from "react";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import AuthContext, { AuthContextProvider } from "./store/AuthContext";
-import Navbar from "./containers/Navbar/Navbar";
 import Dashboard from "./containers/Dashboard/Dashboard";
 import Login from "./containers/Login/Login";
 import Signup from "./containers/Signup/Signup";
@@ -13,11 +13,12 @@ function App() {
   if (token) {
     authCtx.login(token);
   }
-
   return (
     <div className="App">
       <AuthContextProvider>
         <BrowserRouter>
+
+<MainLayout status="hover">
           <Routes>
             <Route
               path="/"
@@ -35,44 +36,33 @@ function App() {
             />
             <Route path="/login" element={<Login />} />
             <Route path="/signup" element={<Signup />} />
-            <Route
-              path="/team"
-              element={
-                // Temp navigation for deploy preview
-                <div>
-                  <h1>Team</h1>
-                </div>
-              }
-            />
-            <Route
-              path="/faq"
-              element={
-                // Temp navigation for deploy preview
-                <div>
-                  <h1>FAQ</h1>
-                </div>
-              }
-            />
-            <Route
-              path="/event"
-              element={
-                // Temp navigation for deploy preview
-                <div>
-                  <h1>Event</h1>
-                </div>
-              }
-            />
-            <Route
-              path="/timeline"
-              element={
-                // Temp navigation for deploy preview
-                <div>
-                  <h1>Timeline</h1>
-                </div>
-              }
-            />
+            <Route path="/team" element={
+              // Temp navigation for deploy preview
+              <div>
+                {/* <h1>Team</h1> */}
+              </div>
+            } />
+            <Route path="/faq" element={
+              // Temp navigation for deploy preview
+              <div>
+                {/* <h1>FAQ</h1> */}
+              </div>
+            } />
+            <Route path="/event" element={
+              // Temp navigation for deploy preview
+              <div>
+                {/* <h1>Event</h1> */}
+              </div>
+            } />
+            <Route path="/timeline" element={
+              // Temp navigation for deploy preview
+              <div>
+                {/* <h1>Timeline</h1> */}
+              </div>
+            } />
           </Routes>
-          <Navbar />
+          </MainLayout>
+          {/* <MainLayout status="hover"/> */}
         </BrowserRouter>
       </AuthContextProvider>
     </div>
