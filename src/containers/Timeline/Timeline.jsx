33cfa95ea3d243fc08cc20a00f251038ce862376
kalashshah <202051096@iiitvadodara.ac.in>
2022-02-20 23:01:19 +0530

    style: add buttons to scroll timeline

--- src/containers/Timeline/Timeline.jsx
+++ src/containers/Timeline/Timeline.jsx
@@ -26,34 +26,44 @@ const useHorizontalScroll = () => {
 const Timeline = () => {
   const scrollRef = useHorizontalScroll();
 
+  const scroll = (offset) => (scrollRef.current.scrollLeft += offset);
+
   return (
-    <div className="timeline" ref={scrollRef}>
-      <div className="timeline__container">
-        <div className="timeline__container__upper">
-          <div className="timeline__container__upper__first">
-            <div className="timeline__container__upper__first__circle">
-              <div className="timeline__container__upper__first__circle__inner" />
+    <>
+      <div className="timeline" ref={scrollRef}>
+        <div className="timeline__container">
+          <div className="timeline__container__upper">
+            <div className="timeline__container__upper__first">
+              <div className="timeline__container__upper__first__circle">
+                <div className="timeline__container__upper__first__circle__inner" />
+              </div>
+              <div className="timeline__container__upper__first__jarvis">
+                <span className="timeline__container__upper__first__jarvis__text">
+                  Jarvis, Initiate Fest
+                </span>
+                <span className="timeline__container__upper__first__jarvis__text">
+                  code : CEREBRO'22
+                </span>
+              </div>
             </div>
-            <div className="timeline__container__upper__first__jarvis">
-              <span className="timeline__container__upper__first__jarvis__text">
-                Jarvis, Initiate Fest
-              </span>
-              <span className="timeline__container__upper__first__jarvis__text">
-                code : CEREBRO'22
-              </span>
-            </div>
+            {timelineData.upper.map((item) => (
+              <TimelineItem top item={item} />
+            ))}
           </div>
-          {timelineData.upper.map((item) => (
-            <TimelineItem top item={item} />
-          ))}
-        </div>
-        <div className="timeline__container__lower">
-          {timelineData.lower.map((item) => (
-            <TimelineItem item={item} />
-          ))}
+          <div className="timeline__container__lower">
+            {timelineData.lower.map((item) => (
+              <TimelineItem item={item} />
+            ))}
+          </div>
         </div>
       </div>
-    </div>
+      <div className="timeline__button__right" onClick={() => scroll(100)}>
+        <hr className="timeline__button__right__line" />
+      </div>
+      <div className="timeline__button__left" onClick={() => scroll(-100)}>
+        <hr className="timeline__button__left__line" />
+      </div>
+    </>
   );
 };
 
