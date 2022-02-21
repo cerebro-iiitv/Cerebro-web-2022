6d7feac4c58aba0b19ccd3de0a0ae7e95f5306c1
kalashshah <202051096@iiitvadodara.ac.in>
2022-02-21 20:38:55 +0530

    feat: configure routing and reset-password

--- src/App.js
+++ src/App.js
@@ -1,84 +1,82 @@
-import MainLayout from "./containers/Layout/Layout";
-import React, { useContext } from "react";
+import React from "react";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
-import AuthContext, { AuthContextProvider } from "./store/AuthContext";
 import Dashboard from "./containers/Dashboard/Dashboard";
+import useAuth from "./hooks/useAuth";
+import MainLayout from "./containers/Layout/Layout";
 import Login from "./containers/Login/Login";
 import Signup from "./containers/Signup/Signup";
 import ForgotPassword from "./containers/ForgotPassword/ForgotPassword";
 import ResetPassword from "./containers/ResetPassword/ResetPassword";
+import ChangePassword from "./containers/ChangePassword/ChangePassword";
 import Timeline from "./containers/Timeline/Timeline";
 import FAQs from "./containers/FAQs/FAQs";
 import Sponsors from "./containers/Sponsors/Sponsors";
-
+import PrivateRoute from "./components/RouteElements/PrivateRoute";
+import UserRoute from "./components/RouteElements/UserRoute";
 import "./App.css";
 
-
-
 function App() {
-  const authCtx = useContext(AuthContext);
+  const auth = useAuth();
   const token = localStorage.getItem("token");
   if (token) {
-    authCtx.login(token);
+    auth.login(token);
   }
 
   return (
     <div className="App">
-      <AuthContextProvider>
-        <BrowserRouter>
-          <MainLayout status="hover">
-            <Routes>
-              <Route
-                path="/"
-                element={
-                  // Temp navigation for deploy preview
-                  <div>
-                    <Link to="/login">Login</Link>
-                    <br />
-                    <Link to="/signup">Signup</Link>
-                    <br />
-                    <Link to="/forgot-password">Forgot Password</Link>
-                    <br />
-                    <Link to="/sponsors">Sponsors</Link>
-                  </div>
-                }
-              />
+      <BrowserRouter>
+        <MainLayout status="hover">
+          <Routes>
+            <Route
+              path="/"
+              element={
+                // Temp navigation for deploy preview
+                <div>
+                  <Link to="/login">Login</Link>
+                  <br />
+                  <Link to="/signup">Signup</Link>
+                  <br />
+                  <Link to="/forgot-password">Forgot Password</Link>
+                  <br />
+                  <Link to="/change-password">Change Password</Link>
+                  <br />
+                  <Link to="/sponsors">Sponsors</Link>
+                </div>
+              }
+            />
+            <Route element={<UserRoute />}>
               <Route path="/login" element={<Login />} />
               <Route path="/signup" element={<Signup />} />
-              <Route path="/sponsors" element={<Sponsors />} />
               <Route path="/forgot-password" element={<ForgotPassword />} />
               <Route
                 path="/account/password-reset/:uidb64/:token"
                 element={<ResetPassword />}
               />
-              <Route
-                path="/team"
-                element={
-                  // Temp navigation for deploy preview
-                  <div>{/* <h1>Team</h1> */}</div>
-                }
-              />
-              <Route
-                path="/faq"
-                element={
-                  // Temp navigation for deploy preview
-
-                  <FAQs />
-                }
-              />
-              <Route
-                path="/event"
-                element={
-                  // Temp navigation for deploy preview
-                  <div>{/* <h1>Event</h1> */}</div>
-                }
-              />
-              <Route path="/timeline" element={<Timeline />} />
-            </Routes>
-          </MainLayout>
-          {/* <MainLayout status="hover"/> */}
-        </BrowserRouter>
-      </AuthContextProvider>
+            </Route>
+            <Route element={<PrivateRoute />}>
+              <Route path="/change-password" element={<ChangePassword />} />
+            </Route>
+            <Route path="/sponsors" element={<Sponsors />} />
+            <Route
+              path="/team"
+              element={
+                // Temp navigation for deploy preview
+                <div>{/* <h1>Team</h1> */}</div>
+              }
+            />
+            <Route path="/faq" element={<FAQs />} />
+            <Route
+              path="/event"
+              element={
+                // Temp navigation for deploy preview
+                <div>{/* <h1>Event</h1> */}</div>
+              }
+            />
+            <Route path="/timeline" element={<Timeline />} />
+          </Routes>
+        </MainLayout>
+        {/* <MainLayout status="hover"/> */}
+      </BrowserRouter>
     </div>
   );
 }
