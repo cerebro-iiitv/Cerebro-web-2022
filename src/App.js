a43a8f416d33d7f3d7ac9ee04a3ad2bc1322c20b
Dhruv Dave <78278281+DhruvDave12@users.noreply.github.com>
2022-02-22 23:39:55 +0530

    Events Page
    * created new branch
    
    * add new reponsive component for scrollbar
    
    * add loading icon, fixed small glitches
    
    * fixing doors p1
    
    * add media query for 1024
    
    * event with background
    
    * add grid in events page
    
    * set margin
    
    * removed unused files and made tablet more responsive
    
    * fixed conflicts organized images and fonts
    
    * fixed conflict
    
    * fc1

--- src/App.js
+++ src/App.js
@@ -16,6 +16,8 @@ import PrivateRoute from "./components/RouteElements/PrivateRoute";
 import UserRoute from "./components/RouteElements/UserRoute";
 import "./App.css";
 
+import Events from "./containers/Events/Events";
+
 function App() {
   const auth = useAuth();
   const token = localStorage.getItem("token");
@@ -25,7 +27,6 @@ function App() {
   if (token) {
     auth.login(token);
   }
-
   return (
     <div className="App">
         <BrowserRouter>
@@ -120,7 +121,7 @@ function App() {
               path="/event"
               element={
                 <MainLayout status="hover">
-                  <div>{/* <h1>Event</h1> */}</div>
+                  <Events />
                 </MainLayout>
               }
             />
