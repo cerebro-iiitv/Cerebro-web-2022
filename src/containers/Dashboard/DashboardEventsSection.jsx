0742f49575abf412457a33141aa71bb06233c545
Madhur Gupta <madhurgupta107@gmail.com>
2022-02-23 14:16:49 +0530

    fix: comments out static data on dashboard

--- src/containers/Dashboard/DashboardEventsSection.jsx
+++ src/containers/Dashboard/DashboardEventsSection.jsx
@@ -2,35 +2,35 @@ import DashboardEventCarousel from "./DashboardEventsCarousel";
 import "./DashboardEventsSection.scss";
 
 // temp data
-const tempeventsData = [
-    {
-        eventName: "Valorant", startDate: "10-03-2022", startTime: "2:00PM", isTeamEvent: true, noMembersInTeam: "3", teamMaxCapacity: "3", teamName: "ValoXD", isTeamFull: true,
-    },
-    {
-        eventName: "Cook-a-code", startDate: "10-03-2022", startTime: "2:00PM", isTeamEvent: false
-    },
-    {
-        eventName: "Hired", startDate: "10-03-2022", startTime: "2:00PM", isTeamEvent: true, noMembersInTeam: "1", teamMaxCapacity: "3", teamName: "ValoXD", isTeamFull: false,
-    },
-    {
-        eventName: "Valorant", startDate: "10-03-2022", startTime: "2:00PM", isTeamEvent: true, noMembersInTeam: "3", teamMaxCapacity: "3", teamName: "ValoXD", isTeamFull: true,
-    },
-    {
-        eventName: "Cook-a-code", startDate: "10-03-2022", startTime: "2:00PM", isTeamEvent: false
-    },
-    {
-        eventName: "Hired", startDate: "10-03-2022", startTime: "2:00PM", isTeamEvent: true, noMembersInTeam: "1", teamMaxCapacity: "3", teamName: "ValoXD", isTeamFull: false,
-    },
-    {
-        eventName: "Valorant", startDate: "10-03-2022", startTime: "2:00PM", isTeamEvent: true, noMembersInTeam: "3", teamMaxCapacity: "3", teamName: "ValoXD", isTeamFull: true,
-    },
-    {
-        eventName: "Cook-a-code", startDate: "10-03-2022", startTime: "2:00PM", isTeamEvent: false
-    },
-    {
-        eventName: "Hired", startDate: "10-03-2022", startTime: "2:00PM", isTeamEvent: true, noMembersInTeam: "1", teamMaxCapacity: "3", teamName: "ValoXD", isTeamFull: false,
-    },
-]
+// const tempeventsData = [
+//     {
+//         eventName: "Valorant", startDate: "10-03-2022", startTime: "2:00PM", isTeamEvent: true, noMembersInTeam: "3", teamMaxCapacity: "3", teamName: "ValoXD", isTeamFull: true,
+//     },
+//     {
+//         eventName: "Cook-a-code", startDate: "10-03-2022", startTime: "2:00PM", isTeamEvent: false
+//     },
+//     {
+//         eventName: "Hired", startDate: "10-03-2022", startTime: "2:00PM", isTeamEvent: true, noMembersInTeam: "1", teamMaxCapacity: "3", teamName: "ValoXD", isTeamFull: false,
+//     },
+//     {
+//         eventName: "Valorant", startDate: "10-03-2022", startTime: "2:00PM", isTeamEvent: true, noMembersInTeam: "3", teamMaxCapacity: "3", teamName: "ValoXD", isTeamFull: true,
+//     },
+//     {
+//         eventName: "Cook-a-code", startDate: "10-03-2022", startTime: "2:00PM", isTeamEvent: false
+//     },
+//     {
+//         eventName: "Hired", startDate: "10-03-2022", startTime: "2:00PM", isTeamEvent: true, noMembersInTeam: "1", teamMaxCapacity: "3", teamName: "ValoXD", isTeamFull: false,
+//     },
+//     {
+//         eventName: "Valorant", startDate: "10-03-2022", startTime: "2:00PM", isTeamEvent: true, noMembersInTeam: "3", teamMaxCapacity: "3", teamName: "ValoXD", isTeamFull: true,
+//     },
+//     {
+//         eventName: "Cook-a-code", startDate: "10-03-2022", startTime: "2:00PM", isTeamEvent: false
+//     },
+//     {
+//         eventName: "Hired", startDate: "10-03-2022", startTime: "2:00PM", isTeamEvent: true, noMembersInTeam: "1", teamMaxCapacity: "3", teamName: "ValoXD", isTeamFull: false,
+//     },
+// ]
 
 const DashboardEventsSection = ({ eventsData }) => {
     return (
@@ -38,8 +38,8 @@ const DashboardEventsSection = ({ eventsData }) => {
             <div className="dashboard-events__title">
                 My Events
             </div>
-            {/* <DashboardEventCarousel eventsData={eventsData} /> */}
-            <DashboardEventCarousel eventsData={tempeventsData} />
+            <DashboardEventCarousel eventsData={eventsData} />
+            {/* <DashboardEventCarousel eventsData={tempeventsData} /> */}
         </div>
     )
 }
