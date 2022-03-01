34691a589464bc162823affcf0514887ab2e41a4
Madhur Gupta <madhurgupta107@gmail.com>
2022-03-02 01:53:59 +0530

    fix: decreases margin/padding on dashboard

--- src/containers/Dashboard/DashboardEventsSection.jsx
+++ src/containers/Dashboard/DashboardEventsSection.jsx
@@ -36,9 +36,10 @@ const DashboardEventsSection = ({ participantDetails, eventsData }) => {
             <div className="dashboard-events__title">
                 My Events
             </div>
-            {!eventsData.length &&
-                <h3 className="dashboard-events__no-events">You haven't registered for any events yet.</h3>}
-            {eventsData.length &&
+            {eventsData.length === 0
+                ?
+                <h3 className="dashboard-events__no-events">You haven't registered for any events yet.</h3>
+                :
                 <>
                     <DashboardEventCarousel
                         eventsData={eventsData}
