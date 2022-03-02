bd15624f58c980b3bf8c0b712697dfb78cd163fa
Madhur Gupta <madhurgupta107@gmail.com>
2022-03-02 13:52:50 +0530

    fix: misc fix for deploy preview

--- src/containers/Navbar/NavTimer.jsx
+++ src/containers/Navbar/NavTimer.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect } from "react";
 
 import navtimer_titles from "../../assets/images/navtimer_titles.svg"
 import "./NavTimer.scss"
