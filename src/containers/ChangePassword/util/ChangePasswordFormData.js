ca16b14bd99458f4c3db371288a29e77c9476ce5
Kalash Shah <81062983+kalashshah@users.noreply.github.com>
2022-03-03 20:40:49 +0530

    Add max char limit to authentication forms (#44)
    * fix: add max char limit to auth forms
    
    * fix: event form bg and form max limit to 1000
    
    * fix: correct image hash and type

--- src/containers/ChangePassword/util/ChangePasswordFormData.js
+++ src/containers/ChangePassword/util/ChangePasswordFormData.js
@@ -25,11 +25,15 @@ export const initialValues = {
 export const validate = (values) => {
   const errors = {};
   if (!values.oldPassword) errors.oldPassword = "Required";
-  else if (values.oldPassword.trim().length < 7)
+  else if (values.oldPassword.length < 7)
     errors.oldPassword = "Password must be at least 7 characters";
+  else if (values.oldPassword.length > 64)
+    errors.oldPassword = "Password must be less than 64 characters";
   if (!values.newPassword) errors.newPassword = "Required";
-  else if (values.newPassword.trim().length < 7)
+  else if (values.newPassword.length < 7)
     errors.newPassword = "Password must be at least 7 characters";
+  else if (values.newPassword.length > 64)
+    errors.newPassword = "Password must be less than 64 characters";
   if (!values.confirmPassword) errors.confirmPassword = "Required";
   else if (values.newPassword !== values.confirmPassword)
     errors.confirmPassword = "Passwords must match";
